a0004dfd4268eee0b8f92b752e80e1d87d5a45dd
Michael Vannucci <vannucci@gmail.com>
2017-07-07 14:06:19 -0400

    Basic server written

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var path = require('path');
+
+var port = 3000;
+
+var app = express();
+
+//Serve up static content, like the CSS files from public directory
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+
+//Set handlebars, set handle
+var exphbs = require('express-handlebars');
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Import routes
+var routes = require("./controllers/burgerController.js");
+
+app.use("/", routes);
+
+app.listen(port);
